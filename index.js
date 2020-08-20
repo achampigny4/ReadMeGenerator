82a07e82f395452c85bd0661077400f21562cc47
Alissa Champigny <achampigny4@gmail.com>
2020-08-20 11:20:03 -0400

    write md file

--- index.js
+++ index.js
@@ -1,68 +1,93 @@
 const inquirer = require("inquirer");
-const { type } = require("os");
+const fs = require("fs");
+const util = require("util");
 
-inquirer
-    .prompt([
+const writeFileAsync = util.promisify(fs.writeFile);
+
+// array of questions for user
+// const questions = inquirer.prompt([
+function promptUser() {
+    return inquirer.prompt([
         {
             type: "input",
-            message: "What is your projects name?",
+            message: "What is your project's name?",
             name: "project"
+        },
+        {
+            type: "list",
+            message: "What licensing does your preject require? ",
+            name: "license",
+            choices: [
+                "MIT",
+                "",
+                "",
+            ]
         },
         {
             type: "input",
             message: "Description of your project: ",
             name: "description"
         },
+        // {
+        //     type: "input",
+        //     message: "Table of Contents: ",
+        //     name: "tableContents"
+        // },
         {
             type: "input",
-            message: "Table of Contents: ",
-            name: "tableContents"
-        },
-        {
-            type: "input",
-            message: "Installation?",
-            name: "installation"
-        },
-        {
-            type: "input",
-            message: "What is your project usage?",
+            message: "What does your project provide for user's?",
             name: "usage"
         },
         {
-            type: "input", //select?
-            message: "Licensing? ",
-            name: "license"
+            type: "list",
+            message: "Command to install: ",
+            name: "installation",
+            choices: ["npm i"],
+            default: "npm i"
+        },
+        {
+            type: "input",
+            message: "Command to run tests: ",
+            name: "test",
+            choices: ["npm test"],
+            default: "npm test"
         },
         {
             type: "input",
             message: "Who contributed to the project? ",
             name: "contributors"
         },
-        {
-            type: "input",
-            message: "Tests: ",
-            name: "tests"
-        },
-        {
-            type: "input",
-            message: "Questions for developer: ",
-            name: "questions"
-        },
-    ]);
+        // {
+        //     type: "input",
+        //     message: "Questions for developer: ",
+        //     name: "questions"
+        // },
+    ])
+};
 
-    // array of questions for user
-const questions = [
-
-];
 
 // function to write README file
-function writeToFile(fileName, data) {
+// function writeToFile(fileName, data) {
+function writeToFile(data) {
+    return `THIS IS A TEMP> PLACE MD TEMPLATE HERE< 
+    ${data.project} ${data.license} ${data.description}`;
 }
+
+const init = async () => {
+    try {
+        const data = await promptUser();
+        const md = writeToFile(data);
+        await writeFileAsync("readMe.md", md);
+        console.log("ReadMe.md file successfully created!");
+
+    } catch (err) {
+        console.log(err);
+    };
+};
 
 // function to initialize program
-function init() {
-
-}
+// function init() {
+// }
 
 // function call to initialize program
 init();
