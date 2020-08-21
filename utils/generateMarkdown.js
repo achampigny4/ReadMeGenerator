4fd54d36f5ae4d2149f118f594189cdc9e552f80
Alissa Champigny <achampigny4@gmail.com>
2020-08-20 21:40:12 -0400

    template update, badge and license functions

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,70 @@
+//create badge
+function renderLicenseBadge(license) {
+  if (license !== "None") {
+    return `![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)`
+  }  return ''
+};
+
+//create license
+function renderLicense() {
+
+};
+
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
-`;
+  ---
+  
+  ${renderLicenseBadge(data.license)}
+  
+  ## Description
+  
+  ---
+  
+  ${data.description}
+  
+  ## Table of Contents:
+  
+  *[Description](#description)
+  *[Usage](#usage)
+  *[License](#license)
+  *[Installation](#installation)
+  *[Test](#test)
+  *[Contribution](#contribution)
+  *[Questions](#questions)
+  
+  ---
+  
+  ## Application use:
+  
+  ${data.usage}
+  
+  ## License:
+  
+  ${renderLicense(data.license)}
+  
+  ## Installation:
+  
+  ${data.installation}
+  
+  ## Test:
+  
+  ${data.test}
+  
+  ## Contribution:
+  
+  Guidlines to contribute:
+  
+  ${data.contribution}
+  
+  ## Questions:
+  
+  Please contact me if you have any questions.
+  email: ${data.email}
+  github: [${data.userName}](www.github.com/${data.userName})
+  
+      `;
 }
 
 module.exports = generateMarkdown;
