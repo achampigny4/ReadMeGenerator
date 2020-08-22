a49ffa669f93aa1d0ee8c95f37f00c6fcf40c0e0
Alissa Champigny <achampigny4@gmail.com>
2020-08-22 15:24:51 -0400

    template update

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,10 +1,7 @@
+//markdown file template for user's input
 // function to generate markdown for README
-
 function generateMarkdown(data) {
-  console.log(data);
   return `# ${data.title}
-
-  ---
   
   $ { renderLicenseBadge(data.license) }
   
@@ -15,43 +12,68 @@ function generateMarkdown(data) {
   ${data.description}
   
   ## Table of Contents:
-  
-  *[Description](#description)
-  *[Usage](#usage)
-  *[License](#license)
-  *[Installation](#installation)
-  *[Test](#test)
-  *[Contribution](#contribution)
-  *[Questions](#questions)
-  
+
   ---
   
-  ## Application use:
+  *[Description](##Description)
+
+  *[Application Use](##usage)
+
+  *[Installation](##Installation)
+
+  *[Test](##Test)
+
+  *[License](##License)
+
+  *[Contribute](##Contribute)
+
+  *[Contact Developer](##Questions)
+  
+  ## Usage:
+
+  ---
   
   ${data.usage}
-  
-  ## License:
-  
-  $ { renderLicense(data.license) }
-  
+
   ## Installation:
+
+  ---
+
+  How to install:
   
   ${data.installation}
   
-  ## Test:
+  ## License:
+
+  ---
   
+  $ { renderLicense(data.license) }
+  [License Source](https://choosealicense.com/licenses/mit/)
+  
+  ## Test:
+
+  ---
+
+  How to test application:
+
   ${data.test}
   
   ## Contribution:
+
+  ---
   
   Guidlines to contribute:
-  
+
   ${data.contribution}
-  
+
   ## Questions:
+
+  ---
   
   Please contact me if you have any questions.
+ 
   email: ${data.email}
+  
   github: [${data.userName}](www.github.com/${data.userName})
   
       `;
